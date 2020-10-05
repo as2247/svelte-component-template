dc2633f96ae1b718578851f42aeffa94b617a71d
Alana Smith <44943819+as2247@users.noreply.github.com>
2020-10-05 17:53:09 +0100

    add navbar

---
+++ src/Accordion.svelte
@@ -0,0 +1,82 @@
+<script>
+    import { slide } from 'svelte/transition';
+    let sections = [
+        {
+            id: 1,
+            title: "Section 1",
+            active: false,
+        },
+        {
+            id: 2,
+            title: "Section 2",
+            active: false,
+        },
+        {
+            id: 3,
+            title: "Section 3",  
+            active: false,
+            
+        }
+    ]
+    
+    const openSection = (section) => {
+    sections = sections.map(button => {
+        button.active = button.id === section.id && !section.active;
+        return button;
+    })
+}
+</script>
+
+<style >
+    .accordion {
+    margin: 24px;
+}
+
+.acc-button {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    height: 60px;
+    font-size: 1.5rem;    
+}
+
+.acc-button:hover {
+    cursor: pointer;
+}
+	
+.slider {
+    padding: 6px;
+    font-size: 20px;
+    font-family: fira-sans,sans-serif;
+}
+
+</style>
+
+<div>
+	<b><p style="font-size: 1.8rem; font-family: fira-sans,sans-serif;">
+		2. Accordion
+		</p></b>
+</div>
+{#each sections as section}
+<div class="accordion">
+    <button class='acc-button' on:click={() => openSection(section) }>{section.title}</button>
+    {#if section.active}
+        <div class="slider" transition:slide>       
+			{#if section.title === 'Section 1' }     
+				<h4>Subtitle 1</h4>
+          		<p>Cras a vehicula orci. Nullam blandit, orci ac volutpat dapibus, ipsum nibh consequat massa, ut laoreet dui ligula ut sem. Sed condimentum volutpat justo. Donec tincidunt at lectus eu interdum. Vivamus pharetra efficitur magna, blandit accumsan orci lacinia non.</p>
+            	<h4>Subtitle 2</h4>
+             	<p>Ut sit amet semper tortor. Etiam ut finibus elit. Sed a rhoncus justo. Vestibulum in facilisis leo. Morbi rhoncus risus est, non gravida nunc porttitor id.</p>
+              	<h4>Subtitle 3</h4>
+               	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet semper tortor. Etiam ut finibus elit. Sed a rhoncus justo. Vestibulum in facilisis leo. Morbi rhoncus risus est, non gravida nunc porttitor id. Cras a vehicula orci. Nullam blandit, orci ac volutpat dapibus, ipsum nibh consequat massa, ut laoreet dui ligula ut sem. Sed condimentum volutpat justo. Donec tincidunt at lectus eu interdum. Vivamus pharetra efficitur magna, blandit accumsan orci lacinia non.</p>             
+               
+			{:else if section.title === 'Section 2'}
+              	<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet semper tortor. Etiam ut finibus elit. Sed a rhoncus justo. Vestibulum in facilisis leo. Morbi rhoncus risus est, non gravida nunc porttitor id. Cras a vehicula orci. Nullam blandit, orci ac volutpat dapibus, ipsum nibh consequat massa, ut laoreet dui ligula ut sem. Sed condimentum volutpat justo. Donec tincidunt at lectus eu interdum. Vivamus pharetra efficitur magna, blandit accumsan orci lacinia non.</p><p> Vestibulum in facilisis leo. Morbi rhoncus risus est, non gravida nunc porttitor id. Cras a vehicula orci.</p>
+                
+            {:else if section.title === 'Section 3'}
+				   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet semper tortor. Etiam ut finibus elit. Sed a rhoncus justo. Vestibulum in facilisis leo. Morbi rhoncus risus est, non gravida nunc porttitor id. Cras a vehicula orci. Nullam blandit, orci ac volutpat dapibus, ipsum nibh consequat massa, ut laoreet dui ligula ut sem. Sed condimentum volutpat justo. Donec tincidunt at lectus eu interdum. Vivamus pharetra efficitur magna, blandit accumsan orci lacinia non.</p>                             
+				   {/if}
+        </div>
+    {/if}
+</div>
+{/each}
---
+++ src/Navbar.svelte
@@ -0,0 +1,127 @@
+<style>  
+        .topnav {
+          overflow: hidden;
+          background-color: #333;
+          width:100%;
+        }
+        
+        .topnav a {
+          float: left;
+          display: block;
+          color: #f2f2f2;
+          text-align: center;
+          padding: 16px;
+          text-decoration: none;
+          font-size: 17px;
+        }
+           
+        .topnav .icon {
+          display: none;
+          padding: 10px;
+        }
+        
+        .responsive {
+          text-align: center;
+        }
+        
+        @media screen and (max-width: 800px) {
+          .topnav a:not(:first-child) {display: none;}
+          .topnav a.icon {
+            float: right;
+            display: block;
+          }
+        }
+        
+        @media screen and (max-width: 800px) {
+          .topnav.responsive {position: relative;}
+          .topnav.responsive .icon {
+            position: absolute;
+            right: 0;
+            top: 0;
+          }
+          .topnav.responsive a {
+            float: none;
+            display: inline-block;
+            text-align: left;
+          }
+        }
+        
+         .acc {
+            margin-bottom: 6px;
+        }
+        
+        .acc-button {
+            display: flex;
+            width: 100%;
+            align-items: center;
+            height: 60px;
+            font-size: 1.5rem;    
+        }
+        
+        .acc-button:hover {
+            cursor: pointer;
+        }
+          
+        .slider {
+            padding: 6px;
+            font-size: 20px;
+            font-family: fira-sans,sans-serif;
+        }
+        
+        .hidden a:hover {
+          background-color: #555;
+        }
+        </style>
+        
+        <script>
+            import { slide } from 'svelte/transition';
+            export let current = '';
+           
+            let sections = [
+                {
+                    title: "Open",
+                    active: false,
+                }
+            ]
+        
+            const expand = (section) => {
+            sections = sections.map(s => {
+                s.active = s.id === section.id && !section.active;
+                return s;
+            })
+        }
+          </script>
+        <div>
+            <b><p style="font-size: 1.8rem; font-family: fira-sans,sans-serif;">1. Responsive Navigation Bar</p></b>
+            <p style='text-align: center'>Try resizing the browser!</p>
+        </div>
+
+        <nav style="margin-top: 16px;" class="standard-text body-text"> 
+            <div class="topnav" id="myTopnav">
+                <a href="#" >NavBar</a> 
+                <a href="index.html" style='float: right;'>Home</a>
+                <a href="#" style='float: right;'>Contact</a>
+                <a href="#" style='float: right;'>About</a>
+    
+    
+                {#each sections as section}
+                    <div class="accn">
+                        <a href='#' class="icon" on:click={() => expand(section) }>MENU</a>
+    
+                {#if section.active}
+                    <div class="slider" transition:slide>
+    
+                        {#if section.title === 'Open' }
+                            <div class='hidden'>
+                                <br /><p><a style='color: white; width: 100%; text-align: center;' href="index.html">Home</a></p>   
+                                <br /><p><a style='color: white; width: 100%; text-align: center;' href="#">Contact</a></p>    
+                                <br /><p><a style='color: white; width: 100%; text-align: center;' href="#">About</a></p>   
+                            </div>
+                        {/if}
+                     </div>
+                {/if}
+               
+              </div>
+              {/each}
+            </div>
+        </nav>
